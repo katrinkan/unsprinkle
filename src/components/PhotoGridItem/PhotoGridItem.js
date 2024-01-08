255818c2f485d4941c5a15f9447dfe4bec324d11
katrinkan <96773609+katrinkan@users.noreply.github.com>
2024-01-08 17:02:57 +0100

    added exercise solution

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -2,10 +2,25 @@ import React from 'react';
 import styled from 'styled-components/macro';
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
+ 
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+        <source
+          type='image/avif'
+          srcSet={`${src.replace('.jpg', '.avif')} 1x, 
+          ${src.replace('.jpg', '@2x.avif')} 2x, 
+          ${src.replace('.jpg', '@3x.avif')} 3x`}/>
+       
+          <source
+          type='image/jpg'
+          srcSet={`${src} 1x, 
+          ${src.replace('.jpg', '@2x.jpg')} 2x, 
+          ${src.replace('.jpg', '@3x.jpg')} 3x`}/>
+        <Image src={src} alt={alt}/>
+        </picture>
+      
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -28,15 +43,20 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+  object-fit: cover; 
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+ white-space: nowrap; 
+ overflow: hidden; 
+ text-overflow: ellipsis; 
+ line-height: 1.5; 
+  
 `;
 
 const Tag = styled.li`
+display: inline; 
+margin-right: 8px; 
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
